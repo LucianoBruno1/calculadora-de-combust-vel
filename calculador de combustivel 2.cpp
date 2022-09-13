0ba0805f96b22e839528f24e74b59d0a34b808ea
Luciano Bruno <70114498+LucianoBruno1@users.noreply.github.com>
2022-09-13 16:38:45 -0300

    Add files via upload

---
+++ calculador de combustivel 2.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main (){
+	
+	
+	float gasolina, alcool;
+
+ gasolina = 0;
+ alcool = 0;
+
+ printf("\n Digite o preço da gasolina\n");
+ scanf("%f", &gasolina);
+
+ printf("\n digite o preço do alcool\n");
+ scanf("%f", &alcool);
+
+ if(alcool <=0.7*gasolina)
+    {
+    printf("\n\n abasteça com alcool");
+ } else {printf("\n\n abasteça com gasolina");
+ }
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
